fa55c53c938297c79489470a1d8473880d924914
garvin tan <garvintan07@gmail.com>
2019-06-21 00:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,17 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  
+  var listItems = document.querySelectorAll('.ranked-list li');
+  for (let item of listItems) {
+    item.innerHTML = parseInt(item.innerHTML) + n;
+  }
 }
+
+function deepestChild() {
+  var current = document.querySelector('div#grand-node');
+  var next = []
+  while (current.children.length !== 0) {
+    current = current.children[0];
+  }
+  return current;
+} 
