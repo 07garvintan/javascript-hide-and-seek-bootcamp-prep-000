b16a1bd4aa664d8edfea2e505bb0e8ece8de70c8
garvin tan <garvintan07@gmail.com>
2019-06-21 00:26:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#next, .target')
+}
+
+function increaseRankBy(n) {
+  var listItems = document.querySelectorAll('.ranked-list li');
+  for (let item of listItems) {
+    item.innerHTML = parseInt(item.innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var current = document.querySelector('div#grand-node');
+  var next = []
+  while (current.children.length !== 0) {
+    current = current.children[0];
+  }
+  return current;
+} 
